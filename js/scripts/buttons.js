4868fc4996abbb31a695af1beb52a6a663a20de4
Wagner Paulo <wagstalos@hotmail.com>
2024-06-18 20:56:31 -0300

    init project

---
+++ js/scripts/buttons.js
@@ -0,0 +1,24 @@
+function menuMobile() {
+  var dotsButton = $(".dots");
+  var menuMobile = $(".menu-mobile");
+  var closeIcon = $(".icon-tabler-x");
+  var links = $(".menu-mobile__links a");
+
+  dotsButton.on("click", function () {
+    menuMobile.slideDown();
+    closeIcon.css("display", "block");
+
+  });
+
+  closeIcon.on("click", function () {
+    menuMobile.slideUp();
+    closeIcon.css("display", "none");
+  });
+
+  links.on("click", function () {
+    menuMobile.slideUp();
+    closeIcon.css("display", "none");
+  });
+}
+
+menuMobile();
