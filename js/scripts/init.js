4cae5c68507cb70f1ca5a919129d9ad0cb2556fe
Wagner Paulo <wagstalos@hotmail.com>
2024-06-21 11:52:50 -0300

    feat: section mission

--- js/scripts/init.js
+++ js/scripts/init.js
@@ -24,5 +24,47 @@ $(document).ready(function () {
     });
   }
 
+  function showMore() {
+    var showChar = 300; // Limite de caracteres
+    var ellipsestext = "...";
+    var moretext = "Ver mais";
+    var lesstext = "Ver menos";
+
+    $(".text-showmore").each(function () {
+      var content = $(this).html();
+      if (content.length > showChar) {
+        var c = content.substr(0, showChar);
+        var h = content.substr(showChar, content.length - showChar);
+
+        var html =
+          c +
+          '<span class="ellipses">' +
+          ellipsestext +
+          '</span><span class="more-text"><span>' +
+          h +
+          '</span></span><span class="show-more">' +
+          moretext +
+          "</span>";
+
+        $(this).html(html);
+      }
+    });
+
+    $(".text-showmore").on("click", ".show-more", function () {
+      if ($(this).hasClass("less")) {
+        $(this).removeClass("less");
+        $(this).html(moretext);
+        $(this).siblings(".more-text").hide();
+        $(this).siblings(".ellipses").show();
+      } else {
+        $(this).addClass("less");
+        $(this).html(lesstext);
+        $(this).siblings(".more-text").show();
+        $(this).siblings(".ellipses").hide();
+      }
+    });
+  }
+
   scrollTarget();
+  showMore();
 });
