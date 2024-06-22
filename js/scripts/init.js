0e2d738dcb60ccee50b0a714a0697893eeaf3cec
Wagner Paulo <wagstalos@hotmail.com>
2024-06-21 22:28:53 -0300

    feat: facilidades

--- js/scripts/init.js
+++ js/scripts/init.js
@@ -65,6 +65,19 @@ $(document).ready(function () {
     });
   }
 
+  $(".accordion-wrap").on("click", function () {
+    $(this).children().eq(1).slideToggle(300);
+    $(this).children().eq(0).toggleClass("accordion-no-bar");
+    $(this).siblings().find(".accordion-header").removeClass("accordion-gold");
+    $(this).siblings().find(".accordion-header .icon").removeClass("rotate-fa");
+    $(this).find(".accordion-header").toggleClass("accordion-gold");
+    $(this).find(".icon").toggleClass("rotate-fa");
+
+    $(".accordion-wrap .accordion-text")
+      .not($(this).children().eq(1))
+      .slideUp(300);
+  });
+
   scrollTarget();
   showMore();
 });
