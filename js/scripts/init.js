0122eea6482000ec313919dd607ea173fe354b1b
Wagner Paulo <wagstalos@hotmail.com>
2024-06-25 11:52:54 -0300

    feat: loding

--- js/scripts/init.js
+++ js/scripts/init.js
@@ -1,3 +1,7 @@
+$(window).on("load", function () {
+  $("body").removeClass("loading");
+});
+
 $(document).ready(function () {
   AOS.init();
 
@@ -21,47 +25,6 @@ $(document).ready(function () {
           });
         }
       });
-    });
-  }
-
-  function showMore() {
-    var showChar = 300; // Limite de caracteres
-    var ellipsestext = "...";
-    var moretext = "Ver mais";
-    var lesstext = "Ver menos";
-
-    $(".text-showmore").each(function () {
-      var content = $(this).html();
-      if (content.length > showChar) {
-        var c = content.substr(0, showChar);
-        var h = content.substr(showChar, content.length - showChar);
-
-        var html =
-          c +
-          '<span class="ellipses">' +
-          ellipsestext +
-          '</span><span class="more-text"><span>' +
-          h +
-          '</span></span><span class="show-more">' +
-          moretext +
-          "</span>";
-
-        $(this).html(html);
-      }
-    });
-
-    $(".text-showmore").on("click", ".show-more", function () {
-      if ($(this).hasClass("less")) {
-        $(this).removeClass("less");
-        $(this).html(moretext);
-        $(this).siblings(".more-text").hide();
-        $(this).siblings(".ellipses").show();
-      } else {
-        $(this).addClass("less");
-        $(this).html(lesstext);
-        $(this).siblings(".more-text").show();
-        $(this).siblings(".ellipses").hide();
-      }
     });
   }
 
