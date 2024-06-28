e17f9aa4b733f6bad428057988763bd8ad42d693
Wagner Paulo <wagstalos@hotmail.com>
2024-06-28 20:52:56 -0300

    fix: qa cliente

--- js/scripts/faq.js
+++ js/scripts/faq.js
@@ -12,3 +12,15 @@ for (i = 0; i < acc.length; i++) {
     }
   });
 }
+
+const root = document.documentElement;
+const marqueeElementsDisplayed = getComputedStyle(root).getPropertyValue(
+  "--marquee-elements-displayed"
+);
+const marqueeContent = document.querySelector("ul.marquee-content");
+
+root.style.setProperty("--marquee-elements", marqueeContent.children.length);
+
+for (let i = 0; i < marqueeElementsDisplayed; i++) {
+  marqueeContent.appendChild(marqueeContent.children[i].cloneNode(true));
+}
